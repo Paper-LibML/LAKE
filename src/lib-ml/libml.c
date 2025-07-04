5b3c741457cf3e0565e4fa64d649f8608acc32af
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2025-07-04 15:02:38 -0500

    style: Remove tabs

--- src/lib-ml/libml.c
+++ src/lib-ml/libml.c
@@ -38,58 +38,58 @@ void print_matrix(int *m, int dim)
 // Test
 int run_dataset_load(void)
 {
-	enum type_t data_type = DOUBLE;
-	struct dataset features, labels;
-	int ret1, ret2;
-	int n_input = 7;
-	int n_output = 1;
+    enum type_t data_type = DOUBLE;
+    struct dataset features, labels;
+    int ret1, ret2;
+    int n_input = 7;
+    int n_output = 1;
 
-	const char *features_path = "/home/gic/Documents/Flank/migration-prediction-notebooks/352-nab_historic_prep_features.csv";
-	const char *labels_path = "/home/gic/Documents/Flank/migration-prediction-notebooks/352-nab_historic_prep_labels.csv";
+    const char *features_path = "/home/gic/Documents/Flank/migration-prediction-notebooks/352-nab_historic_prep_features.csv";
+    const char *labels_path = "/home/gic/Documents/Flank/migration-prediction-notebooks/352-nab_historic_prep_labels.csv";
 
-	/* initialize feature and label datasets from .csv files */
-	if (dataset_from_csv(&features, features_path, ",", n_input, data_type, 0) < 0 || 
-		dataset_from_csv(&labels, labels_path, ",", n_output, data_type, 0)) {
-		return -1;
-	}
+    /* initialize feature and label datasets from .csv files */
+    if (dataset_from_csv(&features, features_path, ",", n_input, data_type, 0) < 0 ||
+        dataset_from_csv(&labels, labels_path, ",", n_output, data_type, 0)) {
+        return -1;
+    }
 
-	// struct norm_metadata *meta = dataset_normalize (&features);
+    // struct norm_metadata *meta = dataset_normalize (&features);
 
-	// // printf ("MIN\t\tRANGE\n");
-	// // for (int i = 0; i < n_input; ++i) {
-	// // 	printf ("%.4f\t\t%.4f\n", meta->min[i], meta->range[i]);
-	// // }
-	// /* Create layers for the model */
-	// struct layer input, hidden1, hidden2, hidden3, output;
+    // // printf ("MIN\t\tRANGE\n");
+    // // for (int i = 0; i < n_input; ++i) {
+    // //   printf ("%.4f\t\t%.4f\n", meta->min[i], meta->range[i]);
+    // // }
+    // /* Create layers for the model */
+    // struct layer input, hidden1, hidden2, hidden3, output;
 
-	// /* initialize each layer */
-	// init_layer (&input, n_input, 8, NONE);
-	// init_layer (&hidden1, 8, 8, SIGMOID);
-	// /*init_layer (&hidden2, 8, 8, SIGMOID);
-	// init_layer (&hidden3, 8, 8, SIGMOID);*/
-	// init_layer (&output, 8, n_output, SIGMOID);
+    // /* initialize each layer */
+    // init_layer (&input, n_input, 8, NONE);
+    // init_layer (&hidden1, 8, 8, SIGMOID);
+    // /*init_layer (&hidden2, 8, 8, SIGMOID);
+    // init_layer (&hidden3, 8, 8, SIGMOID);*/
+    // init_layer (&output, 8, n_output, SIGMOID);
 
-	// struct layer hidden_layers[1] = {hidden1, /*hidden2, hidden3*/};
+    // struct layer hidden_layers[1] = {hidden1, /*hidden2, hidden3*/};
 
-	// /* initialize model */
-	// struct model m;
-	// init_model (&m, 1, n_input, n_output, BCE, hidden_layers, &output, &input, 0, NULL);
+    // /* initialize model */
+    // struct model m;
+    // init_model (&m, 1, n_input, n_output, BCE, hidden_layers, &output, &input, 0, NULL);
 
-	// struct dataset x_train, x_test, y_train, y_test;
-	// const float learning_rate = 0.1;
-	// const int epochs = 1;
-	
-	// int dataset_len = features.size;
-	// int train_len = 0.7 * dataset_len;
+    // struct dataset x_train, x_test, y_train, y_test;
+    // const float learning_rate = 0.1;
+    // const int epochs = 1;
 
-	// x_train = dataset_slice (&features, 0, train_len, -1, -1);
-	// y_train = dataset_slice (&labels, 0, train_len, -1, -1);
-	// x_test = dataset_slice (&features, train_len, dataset_len, -1, -1);
-	// y_test = dataset_slice (&labels, train_len, dataset_len, -1, -1);
+    // int dataset_len = features.size;
+    // int train_len = 0.7 * dataset_len;
 
-	// train (&m, &x_train, &y_train, learning_rate, epochs);
+    // x_train = dataset_slice (&features, 0, train_len, -1, -1);
+    // y_train = dataset_slice (&labels, 0, train_len, -1, -1);
+    // x_test = dataset_slice (&features, train_len, dataset_len, -1, -1);
+    // y_test = dataset_slice (&labels, train_len, dataset_len, -1, -1);
 
-	// model_to_kspace (meta, &m);
+    // train (&m, &x_train, &y_train, learning_rate, epochs);
+
+    // model_to_kspace (meta, &m);
     return 0;
 }
 
